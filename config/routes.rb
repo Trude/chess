f5606a57c9ab87d2ac623330041a04ccd0cf1d9f
Trude Martinsen <trude.martinsen@gmail.com>
2014-08-24 14:01:11 +0200

    mer bah

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'registration#index'
+  root 'registrations#index'
 
   resources :registrations, only: [:index, :create, :update, :destroy]
 

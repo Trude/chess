82cc7e209c7cf2f6ab90308766981de408e80375
Urname2 <jorgenhb@live.com>
2015-09-07 11:24:30 +0200

    NDC 2015 Change

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,6 +1,6 @@
 class Registration < ActiveRecord::Base
 
   validates_presence_of :name, :company, :phone, :email, message: "må fylles ut"
-  validates_acceptance_of :goingToJz, accept: true, message: "Du kan ikke delta i konkurransen hvis du ikke skal på NDC"
+  validates_acceptance_of :goingToJz, accept: true, message: "Du kan ikke delta i konkurransen hvis du ikke skal på JavaZone"
 
 end
